36bd0dda738a6b5ef48c0a93073d1ef8417ce26f
yuuki008 <s22ah261@gmail.com>
2024-06-16 23:00:50 +0900

    auth

---
+++ src/auth.py
@@ -0,0 +1,17 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_spotify_client():
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+        scope="user-modify-playback-state user-read-playback-state"
+    ))
+    print(sp.me())
+    return sp
+
