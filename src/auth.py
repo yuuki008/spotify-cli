c1cc0125166d170c6bce0d23d3d7f2b42bf6b9fe
yuuki008 <s22ah261@gmail.com>
2024-06-17 00:55:40 +0900

    auth user-top-read

--- src/auth.py
+++ src/auth.py
@@ -12,7 +12,7 @@ CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
 def get_spotify_client():
-    scope = "user-read-playback-state,user-modify-playback-state,user-read-currently-playing"
+    scope = "user-read-playback-state,user-modify-playback-state,user-read-currently-playing,user-top-read"
     return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope))
 
 def refresh_token():
